c5543672a0be582ca5c0893c267678da23d04d86
koby <jacov.g@gmail.com>
2022-06-11 13:27:08 +0300

    Give it a try.

---
+++ main.py
@@ -0,0 +1,44 @@
+import codecs
+import tkinter.filedialog
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import tkinter as tk
+from tkinter import filedialog as fd
+
+from multiprocessing.pool import ThreadPool
+
+
+# For selecting file name
+root = tk.Tk()
+root.withdraw()
+
+# For identyfing the system - Clear Screen order
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#print('Please select a Bookmarks file to clean: ')
+in_file = fd.askopenfilename(title='Please select xxxxx: ')
+in_directory = fd.askdirectory()
+
+
+print('Please select a Target file: ')
+out_file = fd.askopenfilename()
+
+soup = BeautifulSoup(codecs.open(in_file, encoding='utf-8'), "html.parser")
+
+with open(out_file, "w", encoding='utf-8') as o_file:
+    def fetch_href(a):
+        try:
+            x = requests.get(a['href'])
+            if x.status_code < 400:
+                print('found One')
+                o_file.write(f"<a href=\"{a['href']}\">{a.text}</a><br>")
+            else:
+                print('A dead one')
+        except (requests.exceptions.Timeout, requests.exceptions.HTTPError,
+                requests.exceptions.ConnectionError, requests.exceptions.InvalidSchema):
+            print('Excepted 1')
+    all_a = soup.find_all('a')
+    with ThreadPool(20) as pool:
+        pool.map(fetch_href, all_a, chunksize=1)
---
+++ main2.py
@@ -0,0 +1,11 @@
+import PyQt5
+from PyQt5.QtWidgets import QFileDialog
+
+
+# def loadFiles(self):
+filter = "TXT (*.txt);;PDF (*.pdf)"
+file_name = QFileDialog()
+    # QtGui.QFileDialog()
+# file_name.setFileMode(QFileDialog.ExistingFiles)
+# names = file_name.getOpenFileNamesAndFilter(self, "Open files", "C\\Desktop", filter)
+print(file_names)
